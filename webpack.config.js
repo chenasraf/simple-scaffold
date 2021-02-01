977288ae5ab841484018857f630875ae5e2e4abe
Chen Asraf <chenasrafil@gmail.com>
2021-02-01 15:24:26 +0200

    build: upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,46 @@
-const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path")
+const webpack = require("webpack")
+const nodeExternals = require("webpack-node-externals")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-  devtool: process.env.NODE_ENV === 'develop' ? 'inline-source-map' : 'source-map',
-  target: 'node',
+  devtool:
+    process.env.NODE_ENV === "develop" ? "inline-source-map" : "source-map",
+  target: "node",
   entry: {
-    index: './scaffold.ts',
-    test: './test.ts',
-    cmd: './cmd.ts',
+    index: "./scaffold.ts",
+    test: "./test.ts",
+    cmd: "./cmd.ts",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    library: 'library',
-    libraryTarget: 'umd',
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    library: "library",
+    libraryTarget: "umd",
   },
   resolve: {
-    extensions: ['.ts']
+    extensions: [".ts"],
   },
   externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: [/\.tsx?$/],
-        loader: 'ts-loader',
-        exclude: [/\/examples\//, /\/node_modules\//]
-      }
-    ]
+        loader: "ts-loader",
+        exclude: [/\/examples\//, /\/node_modules\//],
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
-      '__dirname': '__dirname'
+      __dirname: "__dirname",
     }),
     new webpack.BannerPlugin({
-      banner: '#!/usr/bin/env node',
+      banner: "#!/usr/bin/env node",
       raw: true,
       include: [/cmd\.js/],
     }),
-    new CopyPlugin(['index.d.ts']),
+    new CopyPlugin({ patterns: ["index.d.ts"] }),
   ],
 }
