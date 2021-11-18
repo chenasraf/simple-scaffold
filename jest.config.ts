01e458ee0c1dbb955b4cc4cda5d76d5e5ac94306
Chen Asraf <chenasrafil@gmail.com>
2021-11-18 13:12:37 +0200

    update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -84,7 +84,7 @@ export default {
   // moduleNameMapper: {},
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
+  modulePathIgnorePatterns: ["<rootDir>/dist"],
 
   // Activates notifications for test results
   // notify: false,
