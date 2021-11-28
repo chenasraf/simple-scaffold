2623b787e6e5583599c4c0d1cb20e6262a97d61d
Chen Asraf <chenasrafil@gmail.com>
2021-11-28 14:07:25 +0200

    fixes + add log level [skip publish]

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+export enum LogLevel {
+  None = 0,
+  Debug = 1,
+  Info = 2,
+  Warning = 3,
+  Error = 4,
+}
+
 export type FileResponseFn<T> = (fullPath: string, basedir: string, basename: string) => T
 
 export type FileResponse<T> = T | FileResponseFn<T>
@@ -5,12 +13,15 @@ export type FileResponse<T> = T | FileResponseFn<T>
 export interface ScaffoldConfig {
   /** The name supplied for the output templates */
   name: string
+  /** Template input files/dirs/glob patterns to use as template input. These will be copied to the output directory. */
   templates: string[]
+  /** Output directory to put scaffolded files in. */
   output: FileResponse<string>
   createSubFolder?: boolean
   data?: Record<string, string>
   overwrite?: FileResponse<boolean>
   quiet?: boolean
+  verbose?: LogLevel
   dryRun?: boolean
 }
 export interface ScaffoldCmdConfig {
