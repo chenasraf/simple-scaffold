134c15d7dc1890cff690ffa71f580c91ca940e58
Chen Asraf <chenasrafil@gmail.com>
2022-02-14 09:06:12 +0200

    fail handlebars parse silently

--- src/scaffold.ts
+++ src/scaffold.ts
@@ -41,7 +41,7 @@ export async function Scaffold({ ...options }: ScaffoldConfig) {
     log(options, LogLevel.Info, "Data:", options.data)
     for (let template of options.templates) {
       try {
-        const _isGlob = template.includes("*")
+        const _isGlob = glob.hasMagic(template)
         if (!_isGlob && !(await pathExists(template))) {
           const err: NodeJS.ErrnoException = new Error(`ENOENT, no such file or directory ${template}`)
           err.code = "ENOENT"
@@ -61,37 +61,34 @@ export async function Scaffold({ ...options }: ScaffoldConfig) {
         log(options, LogLevel.Debug, "before glob")
         const files = await promisify(glob)(template, {
           dot: true,
-          debug: false,
-          nodir: options.verbose === LogLevel.Debug,
-          nobrace: true,
-          noext: true,
-          nocomment: true,
-          nonegate: true,
+          debug: options.verbose === LogLevel.Debug,
+          nodir: true,
         })
         log(options, LogLevel.Debug, "after glob")
         for (const inputFilePath of files) {
-          if (!(await isDir(inputFilePath))) {
-            const relPath = makeRelativePath(path.dirname(removeGlob(inputFilePath).replace(_nonGlobTemplate, "")))
-            const basePath = path
-              .resolve(process.cwd(), relPath)
-              .replace(process.cwd() + "/", "")
-              .replace(process.cwd(), "")
-            log(
-              options,
-              LogLevel.Debug,
-              `\nprocess.cwd(): ${process.cwd()}`,
-              `\norigTemplate: ${origTemplate}`,
-              `\nrelPath: ${relPath}`,
-              `\ntemplate: ${template}`,
-              `\ninputFilePath: ${inputFilePath}`,
-              `\nnonGlobTemplate: ${_nonGlobTemplate}`,
-              `\nbasePath: ${basePath}`,
-              `\nisDir: ${_isDir}`,
-              `\nisGlob: ${_isGlob}`,
-              `\n`
-            )
-            await handleTemplateFile(inputFilePath, basePath, options, options.data)
+          if (await isDir(inputFilePath)) {
+            continue
           }
+          const relPath = makeRelativePath(path.dirname(removeGlob(inputFilePath).replace(_nonGlobTemplate, "")))
+          const basePath = path
+            .resolve(process.cwd(), relPath)
+            .replace(process.cwd() + "/", "")
+            .replace(process.cwd(), "")
+          log(
+            options,
+            LogLevel.Debug,
+            `\nprocess.cwd(): ${process.cwd()}`,
+            `\norigTemplate: ${origTemplate}`,
+            `\nrelPath: ${relPath}`,
+            `\ntemplate: ${template}`,
+            `\ninputFilePath: ${inputFilePath}`,
+            `\nnonGlobTemplate: ${_nonGlobTemplate}`,
+            `\nbasePath: ${basePath}`,
+            `\nisDir: ${_isDir}`,
+            `\nisGlob: ${_isGlob}`,
+            `\n`
+          )
+          await handleTemplateFile(inputFilePath, basePath, options, options.data)
         }
       } catch (e: any) {
         handleErr(e)
@@ -112,12 +109,12 @@ async function handleTemplateFile(
   return new Promise(async (resolve, reject) => {
     try {
       const inputPath = path.resolve(process.cwd(), templatePath)
-      const outputPathOpt = getOptionValueForFile(inputPath, data, options.output)
+      const outputPathOpt = getOptionValueForFile(options, inputPath, data, options.output)
       const outputDir = path.resolve(
         process.cwd(),
         ...([outputPathOpt, basePath, options.createSubFolder ? options.name : undefined].filter(Boolean) as string[])
       )
-      const outputPath = handlebarsParse(path.join(outputDir, path.basename(inputPath)), data)
+      const outputPath = handlebarsParse(options, path.join(outputDir, path.basename(inputPath)), data).toString()
       log(
         options,
         LogLevel.Debug,
@@ -129,7 +126,7 @@ async function handleTemplateFile(
         `\nFull output path: ${outputPath}`,
         `\n`
       )
-      const overwrite = getOptionValueForFile(inputPath, data, options.overwrite ?? false)
+      const overwrite = getOptionValueForFile(options, inputPath, data, options.overwrite ?? false)
       const exists = await pathExists(outputPath)
 
       await createDirIfNotExists(path.dirname(outputPath), options)
@@ -140,7 +137,7 @@ async function handleTemplateFile(
           log(options, LogLevel.Info, `File ${outputPath} exists, overwriting`)
         }
         const templateBuffer = await readFile(inputPath)
-        const outputContents = handlebarsParse(templateBuffer, data)
+        const outputContents = handlebarsParse(options, templateBuffer, data)
 
         if (!options.dryRun) {
           await writeFile(outputPath, outputContents)
