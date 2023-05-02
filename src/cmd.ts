7b6260caf8e995310922703331cdd5509922a55b
Chen Asraf <chenasrafil@gmail.com>
2023-05-02 18:33:03 +0300

    docs: update docs

--- src/cmd.ts
+++ src/cmd.ts
@@ -130,7 +130,7 @@ export async function parseCliArgs(args = process.argv.slice(2)) {
           `Version:  ${pkg.version}`,
           `Copyright © Chen Asraf 2017-${new Date().getFullYear()}`,
           ``,
-          `Documentation: ${chalk.underline`https://casraf.dev/simple-scaffold`}`,
+          `Documentation: ${chalk.underline`https://chenasraf.github.io/simple-scaffold`}`,
           `NPM: ${chalk.underline`https://npmjs.com/package/simple-scaffold`}`,
           `GitHub: ${chalk.underline`https://github.com/chenasraf/simple-scaffold`}`,
         ].join("\n"),
