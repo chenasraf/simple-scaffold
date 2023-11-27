807c3e27e2c52a4bd79890c5b1b0c4a5313afe20
Chen Asraf <casraf@pm.me>
2023-11-28 00:45:59 +0200

    ci: update release config

--- release.config.js
+++ release.config.js
@@ -1,23 +1,8 @@
-const releaseRules = [
-  { type: "feat", section: "Features", release: "minor" },
-  { type: "revert", section: "Features", release: "minor" },
-  { type: "fix", section: "Bug Fixes", release: "patch" },
-  { type: "chore", section: "Misc", release: "patch" },
-  { type: "refactor", section: "Misc", release: "patch" },
-  { type: "perf", section: "Misc", release: "patch" },
-  { type: "build", section: "Build", release: "patch" },
-  { type: "docs", section: "Build", release: false },
-  { type: "test", section: "Tests", release: "patch" },
-]
-
 /** @type {import('semantic-release').Options} */
 module.exports = {
-  branches: ["+([0-9])?(.{+([0-9]),x}).x", "master", "next", "next-major", { name: "pre", prerelease: true }],
+  branches: ["master", { name: "pre", prerelease: true }],
   analyzeCommits: {
     path: "semantic-release-conventional-commits",
-    majorTypes: releaseRules.filter((x) => x.release === "major").map((x) => x.type),
-    minorTypes: releaseRules.filter((x) => x.release === "minor").map((x) => x.type),
-    patchTypes: releaseRules.filter((x) => x.release === "patch").map((x) => x.type),
   },
   plugins: [
     [
@@ -27,7 +12,6 @@ module.exports = {
         parserOpts: {
           noteKeywords: ["breaking:", "breaking-fix:", "breaking-feat:"],
         },
-        releaseRules: releaseRules,
       },
     ],
     [
@@ -36,7 +20,6 @@ module.exports = {
         preset: "conventionalcommits",
         parserOpts: {
           noteKeywords: ["breaking", "major"],
-          types: releaseRules,
         },
       },
     ],
