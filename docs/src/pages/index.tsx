4821be6a00eec26b28e17ed5edb12cd21e85dd06
Chen Asraf <casraf@pm.me>
2024-01-29 01:02:07 +0200

    docs: update

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -17,11 +17,11 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/usage">
-            Usage
-          </Link>
           <Link className="button button--secondary button--lg" to="/docs/api">
             API
+          </Link>
+          <Link className="button button--secondary button--lg" to="/docs/usage">
+            Usage
           </Link>
         </div>
       </div>
