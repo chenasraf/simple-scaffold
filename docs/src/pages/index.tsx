e0c0f5c1ce1526b4148a82071b6656638eb3420f
Chen Asraf <casraf@pm.me>
2024-01-29 03:15:32 +0200

    docs: gtag + update deps

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -32,7 +32,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext()
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={siteConfig.title} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
