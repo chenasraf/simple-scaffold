4d35304b3c835f6af6f8c2ba404cf990dfbdb9b5
Chen Asraf <casraf@pm.me>
2024-02-21 02:29:21 +0200

    feat: `list` command

--- release.config.js
+++ release.config.js
@@ -13,20 +13,14 @@ module.exports = {
       "@semantic-release/npm",
       {
         // only update the pkg version on root, don't publish
+        // this is to keep package.json version in sync with the release
         npmPublish: false,
       },
     ],
-    // [
-    //   '@semantic-release/npm',
-    //   {
-    //     // only update the pkg version on doc, don't publish
-    //     npmPublish: false,
-    //     pkgRoot: 'doc',
-    //   },
-    // ]
     [
       "@semantic-release/exec",
       {
+        // pack the dist folder, during publish step (after version was bumped)
         publishCmd: 'echo "Packing..."; cd ./dist && pnpm pack --pack-destination=../; echo "Done"',
       },
     ],
@@ -34,10 +28,12 @@ module.exports = {
       "@semantic-release/npm",
       {
         // publish from dist dir instead of root
+        // this is the actual uild output
         pkgRoot: "dist",
       },
     ],
     [
+      // Release to GitHub
       "@semantic-release/github",
       {
         assets: ["*.tgz"],
@@ -45,6 +41,7 @@ module.exports = {
     ],
     branch === "master"
       ? [
+        // Update CHANGELOG.md only on master
         "@semantic-release/changelog",
         {
           changelogFile: "CHANGELOG.md",
@@ -53,6 +50,7 @@ module.exports = {
       ]
       : undefined,
     [
+      // Commit the package.json and CHANGELOG.md files to git (if modified)
       "@semantic-release/git",
       {
         assets: ["package.json", "CHANGELOG.md"].filter(Boolean),
